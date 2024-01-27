a79e9c6deee299cbbfc2530655876643a901cd43
VRZ77 <157883512+VRZ77@users.noreply.github.com>
2024-01-27 17:37:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,63 @@ const papers = Array.from(document.querySelectorAll('.paper'));
 papers.forEach((paper) => {
   new Paper(paper);
 });
+
+let highestZ = 1;
+
+class Paper {
+  holdingPaper = false;
+  touchStartX = 0;
+  touchStartY = 0;
+  prevTouchX = 0;
+  prevTouchY = 0;
+  velX = 0;
+  velY = 0;
+  rotation = Math.random() * 30 - 15;
+  currentPaperX = 0;
+  currentPaperY = 0;
+
+  init(paper) {
+    paper.addEventListener('pointermove', (e) => {
+      e.preventDefault();
+      if (this.holdingPaper) {
+        this.touchMoveX = e.clientX || e.touches[0].clientX;
+        this.touchMoveY = e.clientY || e.touches[0].clientY;
+
+        this.velX = this.touchMoveX - this.prevTouchX;
+        this.velY = this.touchMoveY - this.prevTouchY;
+
+        this.currentPaperX += this.velX;
+        this.currentPaperY += this.velY;
+
+        paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
+      }
+
+      this.prevTouchX = this.touchMoveX;
+      this.prevTouchY = this.touchMoveY;
+    });
+
+    paper.addEventListener('pointerdown', (e) => {
+      if (this.holdingPaper) return;
+      this.holdingPaper = true;
+
+      paper.style.zIndex = highestZ;
+      highestZ += 1;
+
+      this.touchStartX = e.clientX || e.touches[0].clientX;
+      this.touchStartY = e.clientY || e.touches[0].clientY;
+      this.prevTouchX = this.touchStartX;
+      this.prevTouchY = this.touchStartY;
+    });
+
+    paper.addEventListener('pointerup', () => {
+      this.holdingPaper = false;
+    });
+  }
+}
+
+const papers = Array.from(document.querySelectorAll('.paper'));
+
+papers.forEach(paper => {
+  const p = new Paper();
+  p.init(paper);
+});
